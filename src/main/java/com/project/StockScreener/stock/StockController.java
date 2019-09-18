16db8ff69e09bb3f7181f8d2e356e594e1c9ac49
robzeh <jn912820@dal.ca>
2019-09-17 21:10:09 -0300

    Modified controller, linked css

--- src/main/java/com/project/StockScreener/stock/StockController.java
+++ src/main/java/com/project/StockScreener/stock/StockController.java
@@ -3,24 +3,24 @@ package com.project.StockScreener.stock;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class StockController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String get() {
-        RestTemplate restTemplate = new RestTemplate();
+        /*RestTemplate restTemplate = new RestTemplate();
         Stock stock = restTemplate.getForObject("https://sandbox.iexapis.com/stable/stock/QQQ/quote?token=Tpk_d6a217598fe344f48fd75d254eb1896f", Stock.class);
-        return stock.toString();
+        return stock.toString();*/
+
+        return "index";
     }
 
-    @GetMapping("/search")
-    public String showStock(@RequestParam(name="stock", required = false, defaultValue = "QQQ") String stock, Model model) {
-        model.addAttribute("stock", stock);
+    @PostMapping("/search")
+    public String showStock(@RequestParam(name="symbol", required = false, defaultValue = "QQQ") String symbol, Model model) {
+        model.addAttribute("stock", symbol);
         return "search";
     }
 
